401b922b48ab35297faa005d2e5f2afb9f80d68a
aryan <aryanhazarika04@gmail.com>
2023-05-06 14:47:10 +0530

    added gcd code

--- gcd.cpp
+++ gcd.cpp
@@ -4,10 +4,26 @@ using namespace std;
 int main()
 {
 
-    int a,b;
+    int a,b; 
     cin>>a>>b;
-    int temp = 
+    int min = a<b? a: b;
+    int max = a>b? a: b;
+    // cout<<max<<" "<<min<<endl;
+    int hcf;
+    
 
+    while(min>0)
+    {
+        if (a%min==0 && b%min==0)
+        {
+            hcf = min;
+            break;
+        }
+        min--;
+        
+    }
+    
+    cout<<"hcf: "<<hcf<<endl;
 
 
 
