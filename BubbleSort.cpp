d7613d33f03c85d9053cf0b03174443fdf131a6a
aryan <aryanhazarika04@gmail.com>
2023-05-18 03:13:50 +0530

    added Bubble Sort

---
+++ BubbleSort.cpp
@@ -0,0 +1,51 @@
+//Sorting an Array (Ascending order) using Bubble Sort Algo.
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int n;
+    cout<<"enter the number of array elements: ";
+    cin>>n;
+    int arr[n];
+    cout<<"enter the array elements: "<<endl;
+
+    //Input Array
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+        
+    }
+
+    
+    //Bubble Sort Processing
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                //Swap the two
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+            
+        }
+        
+    }
+    
+    
+    //Displaying the sorted Array
+    cout<<"Sorted array in ascending order is: "<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+
+
+return 0;
+}
